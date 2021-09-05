039fe9c0a4f2cad1c723c12df683fa71697830df
Christian Carnahan <carnhy@bentley.cs.colostate.edu>
2021-09-05 15:15:19 -0600

    Adding Christian Carnahan Info and Image

--- client/src/components/About/teamInfo.js
+++ client/src/components/About/teamInfo.js
@@ -1,20 +1,21 @@
 import memberPic from "../../static/images/Placeholder.jpg";
+import christianImage from "../../static/images/ChristianImage.jpg";
 import teamPic from "../../static/images/PlaceholderTeam.jpg";
 
 export const teamData =
-    {
-        teamName: "Team Name",
-        missionStatement: "Mission Statement",
-        imagePath: teamPic,
-    };
+{
+    teamName: "Team Name",
+    missionStatement: "Mission Statement",
+    imagePath: teamPic,
+};
 
 
 export const memberData = [
     {
-        name: "Team Member 1",
-        bio: "Bio Here",
-        homeTown: "Hometown",
-        imagePath: memberPic
+        name: "Christian Carnahan",
+        bio: "My name is Christian Carnahan, I am a Computer Science major from Boise, I am a High Jumper on the track and Field team here at CSU. In my free time I enjoy working on my Jeep as well as golfing, climbing, and mountain biking.",
+        homeTown: "Boise, Idaho",
+        imagePath: christianImage
     },
     {
         name: "Team Member 2",
