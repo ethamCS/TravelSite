d5a0d03cc045ff3decb2a311c7fc00c019f842a9
Evan Ide <eide1992@colostate.edu>
2021-09-22 02:31:15 +0000

    #203 Streamline the DatabaseConnection class

--- server/src/main/java/com/tco/database/DatabaseConnection.java
+++ server/src/main/java/com/tco/database/DatabaseConnection.java
@@ -1,1 +1,18 @@
+package com.tco.database;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DatabaseConnection {
+
+    public static Connection con;
+
+    public static void connect() {
+        try {
+            Credentials db = new Credentials();
+            con = DriverManager.getConnection(db.getURL(), db.getUSER(), db.getPASSWORD());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
