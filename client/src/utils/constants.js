ac80ce3fb80216722d79ec87851f22a7c1f5ec52
Dylan Armstrong <dylan2@colostate.edu>
2021-09-22 11:50:32 -0600

    update starting location to the oval instead of the parking lot

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -1,4 +1,5 @@
 import ulog from 'ulog';
+import { reverseGeocode } from './reverseGeocode';
 
 export function setLogLevelIfDefault() {
     const urlString = window.location.search;
@@ -20,4 +21,4 @@ export const CLIENT_TEAM_NAME = "t17 We Push to Main";
 
 export const EARTH_RADIUS_UNITS_DEFAULT = { "miles": 3959 };
 
-export const DEFAULT_STARTING_PLACE = { latitude: 40.5734, longitude: -105.0865 };
+export const DEFAULT_STARTING_PLACE = { latitude: 40.5764, longitude: -105.0807 }; //originally 40.5734, -105.0865 reverseGeocode({lat: 40.5764, lng: -105.0807});
