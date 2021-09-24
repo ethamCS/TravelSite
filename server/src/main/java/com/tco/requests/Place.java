2a3234e6661eaf3e87f2f49ecd4efebf344e7da8
Evan Ide <eide1992@colostate.edu>
2021-09-24 14:54:18 +0000

    Add constructor to Place class

--- server/src/main/java/com/tco/requests/Place.java
+++ server/src/main/java/com/tco/requests/Place.java
@@ -4,5 +4,10 @@ import java.util.HashMap;
 
 public class Place extends HashMap<String,String> {
 
+    public Place() {
+        this.put("name", "");
+        this.put("latitude", "");
+        this.put("longitude", "");
+    }
 
 }
