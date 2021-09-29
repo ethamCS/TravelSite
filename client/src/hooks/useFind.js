871658c09908702ebbb6e069faf563d7ca16528e
Evan Ide <eide1992@colostate.edu>
2021-09-29 04:02:20 +0000

    #277 Implement useFind hook initial features

--- client/src/hooks/useFind.js
+++ client/src/hooks/useFind.js
@@ -1,1 +1,18 @@
+import React, { useEffect, useState, useRef } from 'react';
+import { getOriginalServerUrl, sendAPIRequest } from '../utils/restfulAPI';
 
+
+export function useFind(value) {
+    const match = value;
+
+    return [value, sendFindRequest];
+
+}
+
+async function sendFindRequest(matchString, searchLimit) {
+    const url = getOriginalServerUrl();
+    const requestBody = {requestType: "find", match: matchString, limit: searchLimit};
+
+    const findResponse = await sendAPIRequest(requestBody, url);
+    return findResponse;
+}
