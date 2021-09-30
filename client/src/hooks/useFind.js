3a4299fd53b55406968073d430d73b9502cceb4c
Evan Ide <eide1992@colostate.edu>
2021-09-30 01:12:34 +0000

    Implement useFind hook with sendFindRequest and getPlaces

--- client/src/hooks/useFind.js
+++ client/src/hooks/useFind.js
@@ -2,10 +2,29 @@ import React, { useEffect, useState, useRef } from 'react';
 import { getOriginalServerUrl, sendAPIRequest } from '../utils/restfulAPI';
 
 
-export function useFind(value) {
-    const match = value;
 
-    return [value, sendFindRequest];
+export function useFind(value) {
+    const [match, updateMatch] = useState(value);
+    
+    function setMatchValue(newValue) {
+        return updateMatch(newValue);
+    }
+
+    return [match, setMatchValue, getPlaces];
+
+}
+
+async function getPlaces(matchString) {
+    if (matchString === "") {
+        return [];
+    }
+    const responseBody = await sendFindRequest(matchString, 0);
+
+    if (responseBody) {
+        return responseBody.places;
+    }
+
+    return [];
 
 }
 
@@ -14,5 +33,6 @@ async function sendFindRequest(matchString, searchLimit) {
     const requestBody = {requestType: "find", match: matchString, limit: searchLimit};
 
     const findResponse = await sendAPIRequest(requestBody, url);
+
     return findResponse;
 }
