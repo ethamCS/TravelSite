f1d184292895201954749edd0accbaab2100adbd
Christian Carnahan <christian@thecarnahans.com>
2021-09-29 19:16:24 -0600

    Implement Where Is

--- client/src/components/WhereIs/WhereIs.js
+++ client/src/components/WhereIs/WhereIs.js
@@ -1,13 +1,20 @@
 import React from "react";
-import { Input, Container, Button, Col, Row, InputGroup, InputGroupAddon, InputGroupText } from 'reactstrap';
+import { useState } from "react";
+import { latLngToPlace } from "../../utils/transformers";
+import { Input, Container, Button, Col, Row, InputGroup, InputGroupAddon, InputGroupText, Collapse } from 'reactstrap';
+import Coordinate from 'coordinate-parser';
 
 export default function WhereIs(props) {
+    const [convertedLatLang, setConvertedLatLang] = useState(null);
+    const [coordinatesInputText, setCoordiantesInputText] = useState('');
+
+    const context = { convertedLatLang, setConvertedLatLang, coordinatesInputText, setCoordiantesInputText };
 
     return (
         <Container>
-            <WhereIsHeader closeWhereIs={props.closeWhereIs} />
-            <WhereIsBody />
-            <WhereIsFooter closeWhereIs={props.closeWhereIs} />
+            <WhereIsHeader closeWhereIs={props.closeWhereIs} context={context} />
+            <WhereIsBody context={context} />
+            <WhereIsFooter context={context} placeActions={props.placeActions} />
         </Container>
     );
 }
@@ -28,6 +35,10 @@ function WhereIsHeader(props) {
 }
 
 function WhereIsBody(props) {
+    const { convertedLatLang, coordinatesInputText } = props.context;
+    const validCoordinates = convertedLatLang != null;
+    const inputBoxEmpty = !coordinatesInputText;
+    const place = convertedLatLang;
     return (
         <InputGroup className="mt-4">
             <InputGroupAddon addonType="prepend">
@@ -35,23 +46,50 @@ function WhereIsBody(props) {
             </InputGroupAddon>
             <Input
                 placeholder="Latitude, Longitude"
-            //onChange={e => processCoordinatesInput(e.target.value, props.context)}
-            //value={coordinatesInputText}
-            //valid={validCoordinates}
-            //invalid={!inputBoxEmpty && !validCoordinates}
+                onChange={e => processCoordinatesInput(e.target.value, props.context)}
+                value={coordinatesInputText}
+                valid={validCoordinates}
+                invalid={!inputBoxEmpty && !validCoordinates}
             />
         </InputGroup>
     );
 }
 
 function WhereIsFooter(props) {
+    const { convertedLatLang } = props.context;
+    const validCoordinates = convertedLatLang != null;
+
     return (
         <Container style={{ paddingTop: '10px', paddingBottom: '10px' }}>
-            <Row>
-                <Col xs="auto">
-                    <Button name="whereIsFind" color="primary" onClick={props.closeWhereIs}>Find</Button>
-                </Col>
-            </Row>
+            <Collapse isOpen={validCoordinates}>
+                <Row>
+                    <Col xs="auto">
+                        <Button name="whereIsFind" color="primary" onClick={() => props.placeActions.append(latLngToPlace(convertedLatLang))}>Find</Button>
+                    </Col>
+                </Row>
+            </Collapse>
         </Container>
     );
+}
+
+function processCoordinatesInput(onChangeEvent, context) {
+    const { setConvertedLatLang, setCoordiantesInputText } = context;
+
+    const inputText = onChangeEvent;
+    const latLang = getCoords(inputText);
+
+    setCoordiantesInputText(inputText);
+    setConvertedLatLang(latLang);
+}
+
+function getCoords(coordinateString) {
+    try {
+        const convertedCoordinates = new Coordinate(coordinateString);
+        return {
+            lat: convertedCoordinates.getLatitude(),
+            lng: convertedCoordinates.getLongitude()
+        };
+    } catch (error) {
+        return null;
+    }
 }
