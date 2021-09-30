a96381c2ff7be84f1f621fb54adce9d1de3d89ed
Evan Ide <eide1992@colostate.edu>
2021-09-30 03:44:37 +0000

    Render results list

--- client/src/components/Find/Results.js
+++ client/src/components/Find/Results.js
@@ -1,46 +1,57 @@
 import React from 'react';
-import {Container, Row, Col} from 'reactstrap';
+import {Container, Row, Col, Table} from 'reactstrap';
+import { latLngToText, placeToLatLng } from '../../utils/transformers';
 
 export default function Results(props) {
+
     return (
-        <Container>
+        <Table responsive striped>
             <ResultHeader />
-            <ResultBody />
-        </Container>
+            <ResultBody placesList={props.placesList}/>
+        </Table>
     );
 }
 
 function ResultHeader(props) {
     return (
-        <Container>
-            <Row>
-                <Col>
-                </Col> 
-            </Row>
-        </Container>
+        <thead>
+            <tr>
+                <th />
+                <th>Results</th>
+            </tr>
+        </thead>
     );
 }
 
 function ResultBody(props) {
+    
     return (
         <tbody>
-            <br/>
-            <h5>Locations Found</h5>
-            <TableRow />
+            {props.placesList.map((place, index) => 
+                <TableRow
+                    key={`table-${JSON.stringify(place)}-${index}`}
+                    place={place}
+                    placeActions={props.placeActions}
+                    index={index}
+                />
+            )}
         </tbody>
     );
 }
 
-function TableRow(props){
+function TableRow(props) {
+    const name = props.place.name ? props.place.name : "-";
+    const location = placeToLatLng(props.place);
+    const newLocation = latLngToText(location);
+
     return (
-        <Container> 
-            <Row>
-                <Col>1</Col>
-                <Col>name</Col>  
-                <Col>region</Col>
-                <Col>country</Col>
-                <Col>lat/lng</Col>        
-            </Row>
-        </Container>
+        <tr onClick={() => props.placeActions.selectIndex(props.index)}>    
+            <th scope="row">{props.index + 1} </th>
+            <td>
+                {name}
+                <br />
+                <small className="text-muted">{newLocation}</small>
+            </td>
+        </tr>
     );
 }
