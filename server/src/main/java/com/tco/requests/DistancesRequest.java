bbd403bfd0031e43e58b12ab3bef9911a8036a20
= <=>
2021-10-05 18:25:34 -0600

    created the ctor

--- server/src/main/java/com/tco/requests/DistancesRequest.java
+++ server/src/main/java/com/tco/requests/DistancesRequest.java
@@ -1,4 +1,18 @@
+package com.tco.requests;
+import java.util.ArrayList;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 
 public class DistancesRequest extends Request{
-    
+    private Places places; 
+    private Float earthRadius;
+    private Integer[] distances; 
+    private String requestType;
+
+    public DistancesRequest(){
+        this.requestType = "distances";
+        this.earthRadius = 6371.0; 
+    }
+
 }
