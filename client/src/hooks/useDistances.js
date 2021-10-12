536c4830738e9c241109920bd2dcba3b7397c89d
Evan Ide <eide1992@colostate.edu>
2021-10-12 22:21:31 +0000

    Implement sendDistancesRequest

--- client/src/hooks/useDistances.js
+++ client/src/hooks/useDistances.js
@@ -3,5 +3,17 @@ import { getOriginalServerUrl, sendAPIRequest } from '../utils/restfulAPI';
 
 export function useDistances(placesList) {
     const [distancesList, setDistancesList] = useState([]);
-    
+
+
+}
+
+
+
+async function sendDistancesRequest(placesList) {
+    const url = getOriginalServerUrl();
+    const requestBody = {requestType: "distances", places: placesList, earthRadius: 6371};
+
+    const distancesResponse = await sendAPIRequest(requestBody, url);
+
+    return distancesResponse;
 }
