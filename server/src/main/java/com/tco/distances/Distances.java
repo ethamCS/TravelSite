8fa0bbdef6e03f9742908babb762735d4179dafe
ethamCS <etham@rams.colostate.edu>
2021-10-12 19:56:26 -0600

    created class for distances

--- server/src/main/java/com/tco/distances/Distances.java
+++ server/src/main/java/com/tco/distances/Distances.java
@@ -1,1 +1,11 @@
+package com.tco.distances;
+import java.util.ArrayList;
+import com.tco.requests.Places;
+import com.tco.requests.Place;
+import com.tco.distances.Calculate; 
+
+public class Distances{
+
+}
+
 
