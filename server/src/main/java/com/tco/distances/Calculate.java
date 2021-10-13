b71d85fb04e67dd556da36a1418ed1b04e3f3b03
ethamCS <etham@rams.colostate.edu>
2021-10-12 20:52:51 -0600

    create Vincenty formula

--- server/src/main/java/com/tco/distances/Calculate.java
+++ server/src/main/java/com/tco/distances/Calculate.java
@@ -28,8 +28,37 @@ public class Calculate {
     }
   
   public Double circleDistance(Place p1, Place p2, Double radius){
-       return 0.0;
+       
+      double foundDistance = 0; 
+
+      place1Latitude = degree2Rad(Double.parseDouble(p1.get("latitude")));
+      place1Longitude = degree2Rad(Double.parseDouble(p1.get("longitude")));
+      place1Name = p1.get("name");
+
+      place2Latitude = degree2Rad(Double.parseDouble(p2.get("latitude")));
+      place2Longitude = degree2Rad(Double.parseDouble(p2.get("longitude")));
+      place2Name = p1.get("name");
+
+      absoluteDifferenceLongitude = absoluteDifferences(place1Longitude, place2Longitude);
+      absoluteDifferenceLatitude = absoluteDifferences(place1Latitude, place2Latitude);
+
+
+      Double numerator1 = Math.pow( (Math.cos(place1Latitude) * Math.sin(absoluteDifferenceLongitude)), 2);
+      Double numerator2 = Math.cos(place1Latitude) * Math.sin(place2Latitude); 
+      Double numerator3 = Math.sin(place1Latitude) * Math.cos(place2Latitude) * Math.cos(absoluteDifferenceLongitude);
+      Double numerator4 = numerator2 - numerator3;
+      Double numeratorFinal = Math.sqrt(numerator1 + Math.pow(numerator4, 2));
+
+      Double denominator0 = Math.sin(place1Latitude) * Math.sin(place2Latitude);
+      Double denominator1 = Math.cos(place1Latitude) * Math.cos(place2Latitude) * Math.cos(absoluteDifferenceLongitude);
+      Double dFinal = denominator0 + denominator1;
+   
+      thetaBBy = Math.atan2(numeratorFinal, dFinal);
+
+      //foundDistance = arctan(radius, thetaBBy);
+      return foundDistance; 
 
     }
+    
 
 }
