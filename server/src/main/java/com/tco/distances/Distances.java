bbabeb6e4699f198e0abdd07930b47aad7f96093
ethamCS <etham@rams.colostate.edu>
2021-10-12 19:57:36 -0600

    create class vars and Ctor

--- server/src/main/java/com/tco/distances/Distances.java
+++ server/src/main/java/com/tco/distances/Distances.java
@@ -5,6 +5,23 @@ import com.tco.requests.Place;
 import com.tco.distances.Calculate; 
 
 public class Distances{
+    private String name; 
+    private Double earthRadius;
+    private Integer latitude; 
+    private Integer longitude;
+    private Integer origin; 
+    private Integer destination; 
+    private Places places;
+    private Double[] distances; 
+  
+  
+    public Distances(Places places, Double earthRadius){
+        this.places = places;
+        this.earthRadius = earthRadius;
+        this.destination = null; 
+        this.origin = null; 
+        this.distances = new Double[places.size()-1];
+    } 
 
 }
 
