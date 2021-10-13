435281b1b97425b4df906fae37feaf4071637608
ethamCS <etham@rams.colostate.edu>
2021-10-12 20:01:12 -0600

    Update Distances.java

--- server/src/main/java/com/tco/distances/Distances.java
+++ server/src/main/java/com/tco/distances/Distances.java
@@ -2,7 +2,7 @@ package com.tco.distances;
 import java.util.ArrayList;
 import com.tco.requests.Places;
 import com.tco.requests.Place;
-import com.tco.distances.Calculate; 
+
 
 public class Distances{
     private String name; 
