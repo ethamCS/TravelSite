e1e781c764cd2f1ce4bea39523244c47f6e654ea
FarhanAxiq <angahmyone@aol.com>
2021-10-14 14:47:52 -0600

    use Math.toRadians function instead of writing our own in Distances

--- server/src/main/java/com/tco/distances/Calculate.java
+++ server/src/main/java/com/tco/distances/Calculate.java
@@ -19,11 +19,7 @@ public class Calculate {
    public long arctan(Double radius, Double centrelAngle){
         return Math.round(centrelAngle * radius);
    }
-  
-   public Double degree2Rad(Double degree){
-       Double radians = (degree * (Math.PI/180));
-       return radians; 
-    }
+
   
     public Double absoluteDifferences(Double num1, Double num2){
         double difference = 0; 
@@ -35,20 +31,20 @@ public class Calculate {
        
       double foundDistance = 0; 
 
-      place1Latitude = degree2Rad(Double.parseDouble(p1.get("latitude")));
-      place1Longitude = degree2Rad(Double.parseDouble(p1.get("longitude")));
+      place1Latitude = Math.toRadians(Double.parseDouble(p1.get("latitude")));
+      place1Longitude = Math.toRadians(Double.parseDouble(p1.get("longitude")));
       place1Name = p1.get("name");
 
-      place2Latitude = degree2Rad(Double.parseDouble(p2.get("latitude")));
-      place2Longitude = degree2Rad(Double.parseDouble(p2.get("longitude")));
-      place2Name = p1.get("name");
+      place2Latitude = Math.toRadians(Double.parseDouble(p2.get("latitude")));
+      place2Longitude = Math.toRadians(Double.parseDouble(p2.get("longitude")));
+      place2Name = p2.get("name");
 
       absoluteDifferenceLongitude = absoluteDifferences(place1Longitude, place2Longitude);
       absoluteDifferenceLatitude = absoluteDifferences(place1Latitude, place2Latitude);
 
 
       Double numerator1 = Math.pow( (Math.cos(place1Latitude) * Math.sin(absoluteDifferenceLongitude)), 2);
-      Double numerator2 = Math.cos(place1Latitude) * Math.sin(place2Latitude); 
+      Double numerator2 = Math.cos(place1Latitude) * Math.sin(place2Latitude);
       Double numerator3 = Math.sin(place1Latitude) * Math.cos(place2Latitude) * Math.cos(absoluteDifferenceLongitude);
       Double numerator4 = numerator2 - numerator3;
       Double numeratorFinal = Math.sqrt(numerator1 + Math.pow(numerator4, 2));
@@ -56,11 +52,11 @@ public class Calculate {
       Double denominator0 = Math.sin(place1Latitude) * Math.sin(place2Latitude);
       Double denominator1 = Math.cos(place1Latitude) * Math.cos(place2Latitude) * Math.cos(absoluteDifferenceLongitude);
       Double dFinal = denominator0 + denominator1;
-   
+
       thetaBBy = Math.atan2(numeratorFinal, dFinal);
 
       foundDistance = arctan(radius, thetaBBy);
-      return foundDistance; 
+      return foundDistance;
 
     }
     
