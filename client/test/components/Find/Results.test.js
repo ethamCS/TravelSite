e2bcc64a55431337209aa4040efe1c499a2c795e
Evan Ide <eide1992@colostate.edu>
2021-10-19 22:55:01 +0000

    Test click Results Row

--- client/test/components/Find/Results.test.js
+++ client/test/components/Find/Results.test.js
@@ -6,14 +6,22 @@ import { MOCK_RESULTS } from "../../sharedMocks";
 import Results from '../../../src/components/Find/Results';
 
 describe('Results', () => {
+    const placeActions = {
+        append: jest.fn()
+    };
 
     beforeEach(() => {
         fetch.resetMocks();
-        render(<Results placesList={MOCK_RESULTS} placeActions={{append: jest.fn()}}/>);
+        render(<Results placesList={MOCK_RESULTS} placeActions={placeActions}/>);
     });
 
     it('renders a cell with given place expected', () => {
         expect(screen.getByRole('cell', { name: /40.0/i }).textContent)
             .toContain('40.00, 50.00');
+    });
+
+    it('clicks TableRow in Results Component', () => {
+        user.click(screen.getByRole('cell', {name: /40.0/i}));
+        expect(placeActions.append).toHaveBeenCalled();
     });
 });
