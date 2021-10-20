44e547371000c8fcef06404112d0b256b4346540
Christian Carnahan <christian@thecarnahans.com>
2021-10-20 15:36:53 -0600

    Added JSON Save File & TripName Improvements

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -24,3 +24,12 @@ export const EARTH_RADIUS_UNITS_DEFAULT = { "miles": 3959 };
 export const DEFAULT_STARTING_PLACE = { latitude: 40.5764, longitude: -105.0807 }; //originally 40.5734, -105.0865 reverseGeocode({lat: 40.5764, lng: -105.0807});
 
 export const SERVER_FEATURES = ["config", "find", "distances"];
+
+export const FILE_FORMATS = ".json, .csv, application/json, text/csv";
+
+export const MIME_TYPE = {
+    JSON: "application/json",
+    CSV: "text/csv",
+    SVG: "image/svg+xml",
+    KML: "application/vnd.google-earth.kml+xml"
+};
