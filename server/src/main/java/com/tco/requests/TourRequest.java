94b2454f5f88fad784f212bbcc68ba059f6700c7
ethamCS <etham@rams.colostate.edu>
2021-11-01 14:50:05 -0600

    Update TourRequest.java

--- server/src/main/java/com/tco/requests/TourRequest.java
+++ server/src/main/java/com/tco/requests/TourRequest.java
@@ -1,3 +1,10 @@
+package com.tco.requests;
+import java.util.ArrayList;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import com.tco.distances.Distances; 
+import com.tco.distances.Calculate; 
+
 public class TourRequest {
     
 }
