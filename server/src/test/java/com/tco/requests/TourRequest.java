e03bf37bf04a42fd44ead4828996b176442b62fb
Ethan Myers <etham@rams.colostate.edu>
2021-11-01 11:31:11 -0600

    added package

--- server/src/test/java/com/tco/requests/TourRequest.java
+++ server/src/test/java/com/tco/requests/TourRequest.java
@@ -1,3 +1,12 @@
+package com.tco.requests;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import com.tco.misc.BadRequestException;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+
 public class TourRequest {
     
 }
