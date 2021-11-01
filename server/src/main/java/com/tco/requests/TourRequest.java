d5844e9e152163a29cf1c195023d615cea984f13
ethamCS <etham@rams.colostate.edu>
2021-11-01 14:55:54 -0600

    Update TourRequest.java

--- server/src/main/java/com/tco/requests/TourRequest.java
+++ server/src/main/java/com/tco/requests/TourRequest.java
@@ -4,7 +4,14 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import com.tco.distances.Distances; 
 import com.tco.distances.Calculate; 
+import com.tco.misc.BadRequestException; 
 
-public class TourRequest {
+public class TourRequest extends Request {
+    
+     private final transient Logger log = LoggerFactory.getLogger(FindRequest.class); 
+    
+     public void buildResponse() throws BadRequestException {
+        log.trace("buildResponse -> {}", this);
+    }
     
 }
