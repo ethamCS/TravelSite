2714d980fe3bdf6d33f74c9920999c516a2a5b32
Ethan Myers <etham@rams.colostate.edu>
2021-11-01 21:28:51 -0600

    tour class

--- server/src/main/java/com/tco/tour/Tour.java
+++ server/src/main/java/com/tco/tour/Tour.java
@@ -1,1 +1,13 @@
+package com.tco.tour;
+import com.tco.distances.Distances; 
+import com.tco.distances.Calculate; 
+import com.tco.requests.Places;
+import com.tco.requests.Place;
 
+public class Tour {
+    private Places places; 
+    private Double earthRadius;
+    public Integer[] distances;  
+    
+
+}
