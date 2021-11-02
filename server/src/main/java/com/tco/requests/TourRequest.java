4fb364fdd73f377b125dbb3e84b9bb6ea0368648
Ethan Myers <etham@rams.colostate.edu>
2021-11-02 11:03:47 -0600

    import tour and class vars

--- server/src/main/java/com/tco/requests/TourRequest.java
+++ server/src/main/java/com/tco/requests/TourRequest.java
@@ -4,11 +4,15 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import com.tco.distances.Distances; 
 import com.tco.distances.Calculate; 
-import com.tco.misc.BadRequestException; 
+import com.tco.misc.BadRequestException;
+import com.tco.tour.Tour; 
 
 public class TourRequest extends Request {
+    private Places places; 
+    private Double earthRadius;
+    private Integer[] distances; 
     
-     private final transient Logger log = LoggerFactory.getLogger(FindRequest.class); 
+    private final transient Logger log = LoggerFactory.getLogger(FindRequest.class); 
     
      public void buildResponse() throws BadRequestException {
         log.trace("buildResponse -> {}", this);
