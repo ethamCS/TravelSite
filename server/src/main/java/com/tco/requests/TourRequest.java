b415362065c4bac8331f580c7f9e86f1b3bafad7
FarhanAxiq <angahmyone@aol.com>
2021-11-07 15:22:53 -0700

    added testing method for the request

--- server/src/main/java/com/tco/requests/TourRequest.java
+++ server/src/main/java/com/tco/requests/TourRequest.java
@@ -18,5 +18,32 @@ public class TourRequest extends Request {
         Tour tour = new Tour(this.earthRadius, this.places);
         log.trace("buildResponse -> {}", this);
     }
+
+     /* The following methods exist only for testing purposes and are not used
+    during normal execution, including the constructor. */
+    public TourRequest() {
+        this.requestType = "tour";
+        this.places = new Places();
+
+        Place place1 = new Place();
+        place1.put("name", "place A");
+        place1.put("latitude", "0");
+        place1.put("longitude", "1");
+
+        Place place2 = new Place();
+        place2.put("name", "place B");
+        place2.put("latitude", "1");
+        place2.put("longitude", "0");
+
+        Place place3 = new Place();
+        place3.put("name", "place C");
+        place3.put("latitude", "1");
+        place3.put("longitude", "1");
+
+        this.places.add(place1);
+        this.places.add(place2);
+        this.places.add(place3);
+        this.earthRadius = 3959.0;
+    }
     
 }
