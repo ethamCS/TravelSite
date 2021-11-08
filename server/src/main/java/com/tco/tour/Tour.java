9a68056dc88725bdfd687b63e60152bbce8cd230
Ethan Myers <etham@rams.colostate.edu>
2021-11-08 12:44:32 -0700

    implemented distance matrix

--- server/src/main/java/com/tco/tour/Tour.java
+++ server/src/main/java/com/tco/tour/Tour.java
@@ -3,11 +3,14 @@ import com.tco.distances.Distances;
 import com.tco.distances.Calculate; 
 import com.tco.requests.Places;
 import com.tco.requests.Place;
+import java.util.Arrays;
+import java.util.ArrayList;
 
 public class Tour {
     private Places places; 
     private Double earthRadius;
     public Integer[] distances;  
+    public int distanceFound;
 
     public Integer[] doubleToIntegerArray(Double[] doubleArray){
         distances = new Integer[doubleArray.length];
@@ -20,6 +23,20 @@ public class Tour {
         int size = this.places.size();
         int distance[][] = new int[size][size];
 
+        for (int i = 0; i < size; i++)
+            for (int j = i; j < size; j++) {
+                if (i == j) {
+                    distance[i][j] = 0;
+                }
+                else{
+                    Calculate calc = new Calculate();
+                    distanceFound = calc.circleDistance(this.places.get(i), this.places.get(j), this.earthRadius);
+                    distance[i][j] = distanceFound;
+                    distance[j][i] = distanceFound;
+                }
+                
+            }
+          
         return distance;             
     }
 
@@ -27,6 +44,7 @@ public class Tour {
     public Tour(Double earthRadius, Places places){
         this.earthRadius = earthRadius; 
         this.places = places;
+       
         
     }
 }
