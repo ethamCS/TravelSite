4220e3baf14c09111782fe9304c43da4c7b089b3
Ethan Myers <etham@rams.colostate.edu>
2021-11-10 12:43:03 -0700

    created timer

--- server/src/main/java/com/tco/tour/Countdown.java
+++ server/src/main/java/com/tco/tour/Countdown.java
@@ -1,3 +1,24 @@
+package com.tco.tour;
+import com.tco.requests.Places;
+import java.util.Arrays;
+import java.util.ArrayList;
+
 public class Countdown {
+
+    public int stop; 
+    public double seconds;
+    public Double countdownStarter;
+    public double endTime;
+    public double startTime;
     
+     public Countdown(double seconds){
+         this.stop = 0; 
+         this.seconds = seconds;
+         this.countdownStarter = ((seconds * 1000 ) /2.0);
+         this.startTime = System.currentTimeMillis();
+         this.endTime = startTime + countdownStarter;
+     }
+     public boolean runner(){
+        return System.currentTimeMillis() < endTime;
+     }
 }
