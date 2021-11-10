7e16c452410ccd2fef9884dd8cea888b5c50c6c7
Ethan Myers <etham@rams.colostate.edu>
2021-11-09 19:08:50 -0700

    updating tour to only create isntance of a tour

--- server/src/main/java/com/tco/tour/Tour.java
+++ server/src/main/java/com/tco/tour/Tour.java
@@ -7,44 +7,15 @@ import java.util.Arrays;
 import java.util.ArrayList;
 
 public class Tour {
-    private Places places; 
-    private Double earthRadius;
-    public Integer[] distances;  
-    public int distanceFound;
-
-    public Integer[] doubleToIntegerArray(Double[] doubleArray){
-        distances = new Integer[doubleArray.length];
-        for(int i = 0; i < doubleArray.length; i++){
-            distances[i] = doubleArray[i].intValue();
-        }
-        return distances;    
-    }
-    public int[][] distancesMatrix(){
-        int size = this.places.size();
-        int distance[][] = new int[size][size];
-
-        for (int i = 0; i < size; i++)
-            for (int j = i; j < size; j++) {
-                if (i == j) {
-                    distance[i][j] = 0;
-                }
-                else{
-                    Calculate calc = new Calculate();
-                    distanceFound = calc.circleDistance(this.places.get(i), this.places.get(j), this.earthRadius);
-                    distance[i][j] = distanceFound;
-                    distance[j][i] = distanceFound;
-                }
-                
-            }
-          
-        return distance;             
-    }
-
+    public Places places; 
+    public Double earthRadius;  
+    public Double response; 
+    public boolean[] visited; 
+    public int[] currentTour; 
 
     public Tour(Double earthRadius, Places places){
         this.earthRadius = earthRadius; 
         this.places = places;
-        distancesMatrix();
        
         
     }
