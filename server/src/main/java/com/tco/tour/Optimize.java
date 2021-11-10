ae739a8a97284170a6faf22359372a066d27ef30
Ethan Myers <etham@rams.colostate.edu>
2021-11-09 19:50:23 -0700

    dsitance matrix skelly

--- server/src/main/java/com/tco/tour/Optimize.java
+++ server/src/main/java/com/tco/tour/Optimize.java
@@ -1,14 +1,23 @@
 package com.tco.tour;
+import com.tco.distances.Calculate; 
+import com.tco.requests.Places;
+import com.tco.requests.Place;
+import java.util.Arrays;
 
 public class Optimize{
     
     public Tour tour; 
     public int shortestTourDistance;
+    public int[][] distance;
     
     
     public Optimize(Tour tour){
         this.tour = tour; 
         this.shortestTourDistance = Integer.MAX_VALUE;
+    }
+
+    public int[][] distancesMatrix(){      
+        return distance;             
     }
 
 }
