d8ba144e18562ed7f7f6656090d441cef1950dae
Ethan Myers <etham@rams.colostate.edu>
2021-11-10 17:09:01 -0700

    helper functins

--- server/src/main/java/com/tco/tour/Optimize.java
+++ server/src/main/java/com/tco/tour/Optimize.java
@@ -5,18 +5,20 @@ import com.tco.requests.Place;
 import java.util.Arrays;
 
 public class Optimize{
-    
+
     public Tour tour; 
-    public int shortestTourDistance;
     public int[][] distance;
     public int distanceFound;
+    public int tourDistance; 
+    public int shortestTourDistance;
+    public int[] shortestTour;  
  
     public Optimize(Tour tour){
         this.tour = tour; 
         this.shortestTourDistance = Integer.MAX_VALUE;
     }
 
-    public int[][] distancesMatrix(){  
+    public int[][] distancesMatrix(){
         int size = tour.places.size();
         distance = new int[size][size];
 
@@ -33,6 +35,28 @@ public class Optimize{
                 }                
             }        
         return distance;             
-    }    
-                
+    }
+
+    public boolean[] resetVisited(){
+        return tour.visited;
+    }
+
+    public int findIndexOfPlace(Place targetCity){
+        return tour.places.indexOf(targetCity);
+
+    }
+
+    public boolean[] updateVisitedByIndex(int index){
+        return tour.visited;
+    }
+
+    public boolean[] updateVisitedByPlace(Place currentPlace){
+ 
+        return tour.visited;
+    }
+
+    public boolean isvalidIndex(int indexOfCurrent, int currentDist, int shortest){
+       return (!tour.visited[indexOfCurrent] && currentDist < shortest);
+    }
+     
 }
