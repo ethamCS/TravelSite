ed65a680257f3e3166e404c3d2873bc74ccfe8bb
FarhanAxiq <angahmyone@aol.com>
2021-11-11 08:23:19 -0700

    negative because we're in the west duh

--- server/src/main/java/com/tco/requests/DummyPlaces.java
+++ server/src/main/java/com/tco/requests/DummyPlaces.java
@@ -40,17 +40,17 @@ public class DummyPlaces{
         Place place1 = new Place();
         place1.put("name", "Start");
         place1.put("latitude", "40.5853");
-        place1.put("longitude", "105.0844");
+        place1.put("longitude", "-105.0844");
 
         Place place2 = new Place();
         place2.put("name", "Middle");
         place2.put("latitude", "40.0150");
-        place2.put("longitude", "105.2705");
+        place2.put("longitude", "-105.2705");
 
         Place place3 = new Place();
         place3.put("name", "Dest");
         place3.put("latitude", "39.7392");
-        place3.put("longitude", "104.9903");
+        place3.put("longitude", "-104.9903");
 
         this.places.add(place1);
         this.places.add(place2);
