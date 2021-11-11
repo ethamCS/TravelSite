8a69f30b386d2a1135ab2384d66a9ae23c895189
FarhanAxiq <angahmyone@aol.com>
2021-11-11 08:21:15 -0700

    change location for tour testing

--- server/src/main/java/com/tco/requests/DummyPlaces.java
+++ server/src/main/java/com/tco/requests/DummyPlaces.java
@@ -39,18 +39,18 @@ public class DummyPlaces{
     public void createTourPlaces(){
         Place place1 = new Place();
         place1.put("name", "Start");
-        place1.put("latitude", "0");
-        place1.put("longitude", "1");
+        place1.put("latitude", "40.5853");
+        place1.put("longitude", "105.0844");
 
         Place place2 = new Place();
         place2.put("name", "Middle");
-        place2.put("latitude", "1");
-        place2.put("longitude", "0");
+        place2.put("latitude", "40.0150");
+        place2.put("longitude", "105.2705");
 
         Place place3 = new Place();
         place3.put("name", "Dest");
-        place3.put("latitude", "1");
-        place3.put("longitude", "1");
+        place3.put("latitude", "39.7392");
+        place3.put("longitude", "104.9903");
 
         this.places.add(place1);
         this.places.add(place2);
