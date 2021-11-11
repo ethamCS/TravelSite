faf03432feb70f143cdec617f0b642561b849471
FarhanAxiq <angahmyone@aol.com>
2021-11-11 08:49:23 -0700

    addeed Tour object in test

--- server/src/test/java/com/tco/tour/TestOptimize.java
+++ server/src/test/java/com/tco/tour/TestOptimize.java
@@ -9,5 +9,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class TestOptimize{
+    private Tour tour;
     
 }
