e2ccbfeac17b5bdc381f8ed87803b0f92f9d1bb2
Evan Ide <eide1992@colostate.edu>
2021-12-01 21:03:27 +0000

    Find nearest neighbor

--- server/src/main/java/com/tco/tour/NewTour.java
+++ server/src/main/java/com/tco/tour/NewTour.java
@@ -17,19 +17,20 @@ public class NewTour {
     private int[] originalTour;
     private int currentTourDistance;
     private int shortestTourDistance;
-    private Double endTime;
+    //private Double endTime;
 
-    public NewTour(Places placesList, Double earthRadius, Double endTime) {
+    public NewTour(Places placesList, Double earthRadius/*, Double endTime*/) {
         this.placesList = placesList;
         this.size = placesList.size();
         this.earthRadius = earthRadius;
         this.currentTour = new int[this.size];
+        this.shortestTour = new int[this.size];
         this.visited = new boolean[this.size];
         this.originalDistance = 0;
         createDistancesMatrix();
         createOriginalTour();
         this.shortestTourDistance = this.currentTourDistance;
-        this.endTime = endTime;
+        //this.endTime = endTime;
     }
 
     private void createDistancesMatrix(){
@@ -56,24 +57,63 @@ public class NewTour {
 
     private void createOriginalTour() {
         this.originalTour = new int[this.size];
-        for (int i = 0; i < this.size; ++i)
+        for (int i = 0; i < this.size; ++i) {
             this.originalTour[i] = i;
             this.shortestTour[i] = i;
+        }
     }
 
-    public Places findBestKNNTour() {
+    private void resetVisited() {
+        for (int i = 0; i < this.size; ++i) {
+            this.visited[i] = false;
+        }
+    }
+
+    public /*Places*/ void findBestKNNTour() {
         for (int i = 0; i < this.size; ++i) {
             createNearestNeighborTour(i);
-            if (this.currentTourDistance < this.shortestTourDistance) {
-                updateShortestTour();
-            }
-
+            // if (this.currentTourDistance < this.shortestTourDistance) {
+            //     updateShortestTour();
+            // }
+            resetVisited();
         }
         // TODO: Rearrange places based on shortest tour and return the new places list
     }
 
     private void createNearestNeighborTour(int startCityIndex) {
+        // Add starting city to tour and mark it visited
+        this.currentTour[startCityIndex] = this.originalTour[startCityIndex];
+        this.visited[startCityIndex] = true;
+
         
+        int currentCityIndex = startCityIndex;
+        int nextCityIndex = (startCityIndex + 1 < this.size) ? startCityIndex + 1 : 0;
+
+        // Loop through unvisited cities and find nearest neighbor
+        for (int i = 1; i < this.size; ++i) {
+            int nearestNeighborDistance = this.distanceMatrix[currentCityIndex][nextCityIndex];
+            
+            System.out.println("");
+            /* Find closest neighbor */
+            for (int j = 0; j < this.size; ++j) {
+                if (this.visited[j]) {
+                    continue;
+                }
+                else {
+                    // Get the distance b/n current city and 
+                    int currentDistance = this.distanceMatrix[currentCityIndex][j];
+                    System.out.println(currentDistance + " " + nearestNeighborDistance);
+                    if (currentDistance < nearestNeighborDistance) {
+                        nearestNeighborDistance = currentDistance;
+                        this.currentTour[i] = j;
+                        this.visited[j] = true;
+                        this.currentTourDistance += currentDistance;
+                        currentCityIndex = (currentCityIndex + 1 < this.size) ? currentCityIndex + 1 : 0;
+                    }
+                }
+            }
+        }
+        testPrint();
     }
 
     private void updateShortestTour() {
@@ -83,6 +123,12 @@ public class NewTour {
     }
 
 
-
+    private void testPrint() {
+        System.out.println("Current Tour");
+        for (int i = 0; i < this.size; ++i) {
+            System.out.print(this.currentTour[i] + " ");
+        }
+        System.out.println("");
+    }
 
 }
