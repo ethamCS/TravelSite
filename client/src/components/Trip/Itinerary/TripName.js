f97ad51c9f28d5e16ff12130b109a26325f9bc10
Dylan Armstrong <dylan2@colostate.edu>
2021-12-01 19:24:08 -0700

    removed old optimize button

--- client/src/components/Trip/Itinerary/TripName.js
+++ client/src/components/Trip/Itinerary/TripName.js
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import { useToggle } from '../../../hooks/useToggle';
-import { Collapse, Button, Tooltip } from 'reactstrap';
-import { FaTachometerAlt } from 'react-icons/fa';
+import { Collapse, Button } from 'reactstrap';
 import LoadTripButton from './LoadTripButton';
 import SaveTripButton from './SaveTripButton';
 import SaveTripModal from '../../SaveTripModal/SaveTripModal';
@@ -11,8 +10,8 @@ export function EditTripName(props) {
     const [collapse, setCollapse] = useState(false);
     const [tripName, setName] = useState("Enter Trip Name");
     const [fileName, setFileName] = useState(tripName);
-    const [tooltipOpen, setToolTipOpen] = useState(false);
     const [showSaveFile, toggleSaveFile] = useToggle(false);
+    const [tooltipOpen, setToolTipOpen] = useState(false);
 
     const toggle = () => setCollapse(!collapse);
 
@@ -20,8 +19,6 @@ export function EditTripName(props) {
         <th>
             <Collapse isOpen={!collapse}>
                 <Button color="none" style={{ cursor: 'pointer' }} onClick={toggle} data-testid="edit-button">{tripName}</Button>
-                <FaTachometerAlt onClick={props.toggleShorter} style={{ cursor: 'pointer' }} id="tooltip-optimize" data-testid="optimize" />
-                <Tooltip placement={'left'} delay={{ show: 500, hide: 100 }} flip isOpen={tooltipOpen} toggle={() => { setToolTipOpen(!tooltipOpen) }} data-testid="tool-tip" target="tooltip-optimize">Optimize Trip!</Tooltip>
                 <LoadTripButton showMessage={props.showMessage} placeActions={props.placeActions} setName={setName} tooltipOpen={tooltipOpen} setToolTipOpen={setToolTipOpen} />
                 <SaveTripButton tripName={tripName} placeActions={props.placeActions} toggleSaveFile={toggleSaveFile} />
                 <SaveTripModal isOpen={showSaveFile} closeSaveTrip={toggleSaveFile} placeActions={props.placeActions} fileName={fileName} setFileName={setFileName} />
