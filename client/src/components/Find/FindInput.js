6b65667f45d2f882d04b538569a9aa976632017e
Dylan Armstrong <dylan2@colostate.edu>
2021-12-07 21:26:32 -0700

    added find dropdown stuff

--- client/src/components/Find/FindInput.js
+++ client/src/components/Find/FindInput.js
@@ -1,6 +1,7 @@
 import React, {useState} from "react";
-import { Button, Col, Collapse, Container } from "reactstrap";
-import { FaDice, FaSearchLocation, FaQuestion } from 'react-icons/fa';
+import { Button, Collapse, ButtonGroup, DropdownItem, DropdownMenu, DropdownToggle, UncontrolledDropdown } from "reactstrap";
+import { FaDice, FaSearchLocation } from 'react-icons/fa';
+import { BiDotsVerticalRounded } from 'react-icons/bi';
 import { FindInputGroup, showRandom } from "./Find";
 
 export function FindInput(props){
@@ -20,8 +21,37 @@ export function FindInput(props){
             <Collapse isOpen={random}>
                 <b>Random Places!  </b>
                 <Button color="primary" data-testid="randomButton" onClick={async () => showRandom(props.context, props.serverSettings, props.setRandom, props.active)}><FaDice/></Button>
+                <FindActionsDropdown toggle={toggle}/>
             </Collapse>
-            <Button onClick={toggle}>Temp</Button>
+            <FindActionsDropdown toggle={toggle}/>
         </div>
+    );
+}
+
+function FindActionsDropdown(props){
+    return (
+        <FindDropdown {...props}>
+            <DropdownItem onClick={props.toggle}>
+                <FaSearchLocation/>
+            </DropdownItem>
+            <DropdownItem onClick={props.toggle}>
+                <FaDice/>
+            </DropdownItem>
+        </FindDropdown>
+    );
+}
+
+function FindDropdown(props) {
+    return (
+        <UncontrolledDropdown direction='left' >
+            <DropdownToggle data-testid={'FindInput Dropdown'} color='none'>
+                <BiDotsVerticalRounded/>
+            </DropdownToggle>
+            <DropdownMenu>
+                <ButtonGroup>
+                    {props.children}
+                </ButtonGroup>
+            </DropdownMenu>
+        </UncontrolledDropdown>
     );
 }
